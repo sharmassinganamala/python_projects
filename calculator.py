a19a19dae12081f09f4fe7f6f14bb6d83d12edb4
sharmassinganamala <singanamalasharmasvali@gmail.com>
2024-06-30 19:15:45 +0530

    calculator projects

---
+++ calculator.py
@@ -0,0 +1,58 @@
+from turtle import clear
+import os
+import sys
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+def add(n1,n2):
+    return n1+n2
+
+def subtract(n1,n2):
+    return n1-n2
+
+def multiply(n1,n2):
+    return n1*n2
+
+def divide(n1,n2):
+    return n1/n2
+
+operator_symbol  = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide
+}
+def calculator():
+    print(logo)
+    num1 = int(input("What is the first number ? "))
+    for symbol in operator_symbol:
+        print(symbol)
+    should_countinue  = True
+    while should_countinue:
+        operator = input("pick an operator from the above? ")
+        num2 = int(input("What is the next number : "))
+        calculation_symbol = operator_symbol[operator]
+        answer = calculation_symbol(num1,num2)
+        print(f"{num1} {operator} {num2} = {answer}")
+        countinue = input("Type y if you want to countinue or Type 'n' want to start again ?").lower()
+        if countinue == "y":
+            num1 = answer 
+        else:
+            should_countinue = False
+            calculator()
+calculator()
+
+        
